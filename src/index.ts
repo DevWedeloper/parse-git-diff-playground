947913cb49919abcb928f5b646f58bc1ab4049f2
DevWedeloper <vicnathangabrielle@gmail.com>
2026-06-30 01:54:24 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import { readFile } from "node:fs/promises";
+import { execSync } from "node:child_process";
 import parseGitDiff from "parse-git-diff";
 
 async function main() {
-  const diff = await readFile("sample.diff", "utf8");
+  const diff = execSync("git diff HEAD~1 HEAD", {
+    encoding: "utf8",
+});
+
+  if (!diff.trim()) {
+    console.log("No changes found.");
+    return;
+  }
 
   const parsed = parseGitDiff(diff);
 
